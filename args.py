544cd727bc158c3128eb8428f6cc08634128d501
Pockonechnyy Eduard <pokonechnyy.ep@phystech.edu>
2019-04-24 12:05:29 +0300

    added starter notebook

--- args.py
+++ args.py
@@ -92,11 +92,13 @@ def get_train_args():
 
     add_common_args(parser)
     add_train_test_args(parser)
-
+   
+    
     parser.add_argument('--eval_steps',
                         type=int,
                         default=50000,
                         help='Number of steps between successive evaluations.')
+
     parser.add_argument('--lr',
                         type=float,
                         default=0.5,
